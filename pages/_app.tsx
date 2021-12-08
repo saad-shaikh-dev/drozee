03eb2ca34398372002a2134284914f4f631c7c8a
Ayush Yadav <ayushyadav468@gmail.com>
2021-12-08 17:56:00 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+
+import 'styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta charSet='utf-8' />
+				<meta httpEquiv='Cache-control' content='public' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
